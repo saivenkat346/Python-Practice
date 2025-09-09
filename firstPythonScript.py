39e8f790e5fe56d54d8d1f64bbe5b5bf4ffd2e17
sai venkat <saisam346@gmail.com>
2025-09-09 10:32:15 +0530

    first practice script

---
+++ firstPythonScript.py
@@ -0,0 +1,14 @@
+# import random
+# def get_choices():
+#     player_choice = "rock"
+#     computer_choice ="paper"
+
+#     list_of_choices = ["rock","paper","seccor"]
+
+#     return list_of_choices[random.randrange(0,list_of_choices.__len__())]
+
+# print(get_choices())
+
+name =  input('Name: ')
+age = input('age: ')
+print("hello", name , "and you are", age )
